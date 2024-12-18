a9a0c2967f330db040d7a5f2d856921b16613cea
Dmitry Maximyuk <d_maksimyk@vk.com>
2024-12-18 17:53:33 +0500

    add gzip compression

--- vite/default.config.ts
+++ vite/default.config.ts
@@ -4,12 +4,13 @@ import path from "node:path";
 import core from "@vitejs/plugin-react-swc";
 import svgr from "vite-plugin-svgr";
 import tsPaths from "vite-tsconfig-paths";
+import { compression } from "vite-plugin-compression2";
 
 export const defaultConfig: InlineConfig = {
   appType: "spa",
   publicDir: "public",
   assetsInclude: ["**/*.tgs"],
-  plugins: [tsPaths(), core(), svgr()],
+  plugins: [tsPaths(), core(), svgr(), compression({ algorithm: "gzip" })],
   resolve: {
     alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
   },
