39f103b8321d5141e5af709bfd25b35c01c193a2
Dmitry Maximyuk <d_maksimyk@vk.com>
2024-12-18 14:20:51 +0500

    Update header and fix typography

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,13 @@
 import "./Header.sass";
 
-import { useEffect, type AllHTMLAttributes, type FC } from "react";
+import { type AllHTMLAttributes, type FC } from "react";
 import { useTranslation } from "i18nano";
+
+import { getRoutePath } from "utils";
 
 import { Link } from "react-router-dom";
 import { Flex, Title, Text } from "uikit";
-import { getRoutePath } from "utils";
+
 import { RouteID } from "models";
 
 export interface HeaderProps extends AllHTMLAttributes<HTMLElement> {}
@@ -21,11 +23,13 @@ const Header: FC<HeaderProps> = () => {
         vertical="center"
       >
         <Link to={getRoutePath(RouteID.Home)}>
-          <Title weight="1">ZLottie</Title>
+          <Title className="Header__text" weight="1">
+            ZLottie
+          </Title>
         </Link>
         <Flex horizontal="end" vertical="center">
           <Link to={getRoutePath(RouteID.HowToUse)}>
-            <Text>Docs</Text>
+            <Text className="Header__text">Docs</Text>
           </Link>
         </Flex>
       </Flex>
