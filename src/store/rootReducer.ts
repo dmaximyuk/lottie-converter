f30a49d534ca90c6051b2d089314a762555e9256
Dmitry Maximyuk <d_maksimyk@vk.com>
2024-12-18 16:53:18 +0500

    refactoring...

--- src/store/rootReducer.ts
+++ src/store/rootReducer.ts
@@ -1,11 +1,9 @@
 import { combineReducers } from "@reduxjs/toolkit";
 
 import { reducer as dndFiles } from "./dndFiles";
-import { reducer as example } from "./example";
 
 export const rootReducer = combineReducers({
   dndFiles,
-  example,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
--- src/store/rootSaga.ts
+++ src/store/rootSaga.ts
@@ -1,8 +1,7 @@
 import { all } from "redux-saga/effects";
 
 import { dndFilesSaga } from "./dndFiles/sagas";
-import { exampleSaga } from "./example/sagas";
 
 export function* rootSaga() {
-  yield all([dndFilesSaga(), exampleSaga()]);
+  yield all([dndFilesSaga()]);
 }
