576d03d792bd22c52cb199111739bd8d0f25307d
Dmitry Maximyuk <d_maksimyk@vk.com>
2024-12-18 17:58:04 +0500

    fix deploy

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -26,6 +26,6 @@ const pages: RouteObject[] = [
 ];
 
 const routes = removeUnnecessary(pages);
-const router = createBrowserRouter(routes, { basename: "/zlottie" });
+const router = createBrowserRouter(routes, { basename: "/" });
 
 export { pages, routes, router };
