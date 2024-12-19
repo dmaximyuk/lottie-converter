6dbac70f018d14ce7678a25ce24f201653df4044
Dmitry Maximyuk <d_maksimyk@vk.com>
2024-12-20 02:37:47 +0500

    fix lang

--- src/vars/app.ts
+++ src/vars/app.ts
@@ -1,6 +1,8 @@
-export const LANG = (localStorage.getItem("lang") ||
-  navigator.language ||
-  "en") as string | undefined;
+export const LANG = (
+  (localStorage.getItem("lang") || navigator.language || "en") as
+    | string
+    | undefined
+)?.slice(0, 2);
 
 export const THEME = (localStorage.getItem("theme") || "light") as
   | "dark"
